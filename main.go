1ed1beb8ebe31081956e284dd71136aed9217c49
Christiaan Burchell <christiaan.burchell@gemalto.com>
2018-09-06 11:02:24 -0700

    format file

--- main.go
+++ main.go
@@ -1,27 +1,26 @@
 package main
 
 import (
-	"github.com/cjburchell/tools-go"
 	"log"
-	"github.com/cjburchell/yasls/web"
+
+	"github.com/cjburchell/tools-go"
 	"github.com/cjburchell/yasls/config"
 	"github.com/cjburchell/yasls/processor"
+	"github.com/cjburchell/yasls/web"
 )
 
 func main() {
 	configFile := tools.GetEnv("CONFIG_FILE", "/config.json")
 
 	err := config.Setup(configFile)
-	if err != nil{
+	if err != nil {
 		log.Print(err.Error())
 	}
 
 	err = processor.Start()
-	if err != nil{
+	if err != nil {
 		log.Print(err.Error())
 	}
 	defer processor.Stop()
 	web.StartHttp()
 }
-
-
